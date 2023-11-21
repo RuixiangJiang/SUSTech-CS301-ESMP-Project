1d00e72d5d7b9860e9668347d7e0396bd5a72913
RuixiangJiang <2037358823@qq.com>
2023-11-21 10:48:38 +0800

    upd calc

--- Core/Src/calc/calc.c
+++ Core/Src/calc/calc.c
@@ -5,56 +5,7 @@
 #include <stdlib.h>
 #include <string.h>
 #include <math.h>
-#include "calc.h"
-
-// Stack structure
-typedef struct {
-    char dataC[20];
-    float dataF[20];
-    int topC, topF;
-} * stackPtr, stack;
-
-// Function to initialize the stack
-stackPtr InitStack() {
-    stackPtr head = (stackPtr)malloc(sizeof(stack));
-    head->topC = -1;
-    head->topF = -1;
-    return head;
-}
-
-// Function to push a character onto the stack
-void pushC(stackPtr head, char c) {
-    if (head->topC >= 18) {
-        printf("No space for characters in the stack.\n");
-        return;
-    }
-    head->topC++;
-    head->dataC[head->topC] = c;
-}
-
-// Function to pop a character from the stack
-char popC(stackPtr head) {
-    char tempchar = head->dataC[head->topC];
-    head->topC--;
-    return tempchar;
-}
-
-// Function to push a float onto the stack
-void pushF(stackPtr head, float c) {
-    if (head->topF >= 18) {
-        printf("No space for floats in the stack.\n");
-        return;
-    }
-    head->topF++;
-    head->dataF[head->topF] = c;
-}
-
-// Function to pop a float from the stack
-float popF(stackPtr head) {
-    float tempfloat = head->dataF[head->topF];
-    head->topF--;
-    return tempfloat;
-}
+#include "../../Inc/calc.h"
 
 // Function to check if a character is a number
 int isNumber(char c) {
