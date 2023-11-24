4f7d1dd4e868e93c996b4d2d9e6ef5493fc63837
RuixiangJiang <2037358823@qq.com>
2023-11-24 18:52:38 +0800

    upd tetris

--- Core/Src/tetris.c
+++ Core/Src/tetris.c
@@ -14,20 +14,25 @@ basicBlock block[10][4];
 unsigned char infoUART[1024] = {0};
 int infoLen;
 
+int COL_N = 100;
+
 void initMap(){
 	memset(map.data, 0, sizeof(map.data));
 	memset(map.color, 0, sizeof(map.color));
 	memset(map.id, 0, sizeof(map.id));
-	for (int i = 0; i <= ROW; i++){ // boundary
+	for (int i = 0; i <= ROW; i++){
 		map.data[i][0] = map.data[i][COL + 1] = 1;
 		map.color[i][0] = map.color[i][COL + 1] = 1;
 		map.id[i][0] = map.id[i][COL + 1] = 0;
 	}
-	for (int j = 0; j <= COL; j++){ // boundary
+	for (int j = 0; j <= COL; j++){
 		map.data[ROW + 1][j] = 1;
 		map.color[ROW + 1][j] = 1;
 		map.id[ROW + 1][j] = 0;
 	}
+    map.data[ROW + 1][COL + 1] = 1;
+    map.color[ROW + 1][COL + 1] = 1;
+    map.id[ROW + 1][COL + 1] = 0;
 }
 
 void initBlock(){
@@ -80,14 +85,16 @@ void initGame(){
 }
 
 // after rotation the (shape) block has (form) and top-left position (x, y)
-void drawBlock(int shape, int form, int id, int x, int y){
+int drawBlock(int shape, int form, int id, int x, int y){
 	for (int i = 0; i < 4; i++)
 		for (int j = 0; j < 4; j++)
 			if (block[shape][form].space[i][j]){
+                if (map.data[x + i][y + j] && map.id[x + i][y + i] != 0 && map.id[x + i][y + i] != id) return 1;
 				map.data[x + i][y + j] = 1;
 				map.color[x + i][y + j] = shape;
 				map.id[x + i][y + j] = id;
 			}
+    return 0;
 }
 
 // use space to cover the block before rotation
@@ -139,18 +146,6 @@ int clearRow(){
 	return flag;
 }
 
-// judge if finish, return 1 means game over
-int judgeFinish(){
-	while (clearRow());
-	for (int j = 1; j <= COL; j++){
-		if (map.data[1][j] == 1){
-			printf("Game Over.\n");
-			return 1;
-		}
-	}
-	return 0;
-}
-
 int inRange(int x, int y, int x0, int x1, int y0, int y1){return x >= x0 && x <= x1 && y >= y0 && y <= y1;}
 char getInstruction(int x, int y){
     if (inRange(x, y, 10, 80, 10, 40)) return 'R';
@@ -163,7 +158,6 @@ char getInstruction(int x, int y){
 }
 
 void printMap(){
-    int COL_N = 100;
     uint16_t COLOR[COL_N + 5];
     for (int i = 0; i <= ROW + 1; i++){
         int x = 10, y = 50 + i * 10;
@@ -209,27 +203,102 @@ void printMap(){
     HAL_Delay(500);
 }
 
+void printNextBlock(int shape, int form){
+    uint16_t COLOR[COL_N + 5];
+    for (int i = 0; i < 4; i++){
+        int x = 190, y = 110 + i * 10;
+        for (int j = 0; j < 4; j++){
+            if (!block[shape][form].space[i][j]){
+                for (int k = 0; k < COL_N; k++) COLOR[k] = WHITE;
+            }
+            else switch (shape){
+                case 0:
+                    for (int k = 0; k < COL_N; k++) COLOR[k] = WHITE;
+                    break;
+                case 2:
+                    for (int k = 0; k < COL_N; k++) COLOR[k] = BLUE;
+                    break;
+                case 3:
+                    for (int k = 0; k < COL_N; k++) COLOR[k] = RED;
+                    break;
+                case 4:
+                    for (int k = 0; k < COL_N; k++) COLOR[k] = GREEN;
+                    break;
+                case 5:
+                    for (int k = 0; k < COL_N; k++) COLOR[k] = YELLOW;
+                    break;
+                case 6:
+                    for (int k = 0; k < COL_N; k++) COLOR[k] = BROWN;
+                    break;
+                case 7:
+                    for (int k = 0; k < COL_N; k++) COLOR[k] = BRED;
+                    break;
+                case 8:
+                    for (int k = 0; k < COL_N; k++) COLOR[k] = MAGENTA;
+                    break;
+                default:
+                    for (int k = 0; k < COL_N; k++) COLOR[k] = RED;
+                    break;
+            }
+            LCD_Color_Fill(x + 2, y + 2, x + 8, y + 8, COLOR);
+            x += 10;
+        }
+    }
+    HAL_Delay(500);
+}
 void startGame(){
 	int shape = rand() % 7 + 2, form = rand() % 4, id = 1;
+    int hasPressed = 0;
+    int hasPaused = 0;
 	while (1){
 		int nxtShape = rand() % 7 + 2, nxtForm = rand() % 4;
 		int x = 1, y = COL / 2 - 2; // initial position of block
 		while (1){
             tp_dev.scan(0);
-			drawBlock(shape, form, id, x, y);
+            if (drawBlock(shape, form, id, x, y)){ // finish
+                LCD_ShowString(35, 155, 200, 80, 24, (uint8_t*) "Game Over!");
+                return;
+            }
             printMap();
+            printNextBlock(nxtShape, nxtForm);
+
+            if (hasPaused){
+                if (tp_dev.sta & TP_PRES_DOWN){
+                    HAL_Delay(10);
+                    if (tp_dev.sta & TP_PRES_DOWN){
+                        if (getInstruction(tp_dev.x[0], tp_dev.y[0]) == 'p'){
+                            hasPaused = 0;
+                        };
+                    }
+                }
+                continue;
+            }
 			
 			char ch = ' '; // input instruction from board
-            int waitCount = 50000;
+            int waitCount = 5;
             while (waitCount--){
+                // if (tp_dev.sta & TP_PRES_DOWN){
+                //     HAL_Delay(50);
+                //     if (tp_dev.sta & TP_PRES_DOWN){
+                //     ch = getInstruction(tp_dev.x[0], tp_dev.y[0]);
+                //     infoLen = snprintf((char *)infoUART, sizeof(infoUART), "(%d, %d) -> ", tp_dev.x[0], tp_dev.y[0]);
+                //     HAL_UART_Transmit(&huart1, infoUART, infoLen, HAL_MAX_DELAY);
+                //     break;
+                //     }
+                // }
                 if (tp_dev.sta & TP_PRES_DOWN){
-                    HAL_Delay(50);
-                    if (tp_dev.sta & TP_PRES_DOWN){
+                    HAL_Delay(10);
+                    if (tp_dev.sta & TP_PRES_DOWN && !hasPressed){
                         ch = getInstruction(tp_dev.x[0], tp_dev.y[0]);
-                        //infoLen = snprintf((char *)infoUART, sizeof(infoUART), "(%d, %d) -> ", tp_dev.x[0], tp_dev.y[0]);
+                        hasPressed = 1;
+                        infoLen = snprintf((char *)infoUART, sizeof(infoUART), "(%d, %d) -> ", tp_dev.x[0], tp_dev.y[0]);
                         //HAL_UART_Transmit(&huart1, infoUART, infoLen, HAL_MAX_DELAY);
                         break;
                     }
+                }
+                else{
+                    HAL_Delay(10);
+                    if (!(tp_dev.sta & TP_PRES_DOWN)) hasPressed = 0;
                 }
             }
             infoLen = snprintf((char *)infoUART, sizeof(infoUART), "input instruction is %c.\r\n", ch);
@@ -244,7 +313,7 @@ void startGame(){
 								map.color[x + i][y + j] = shape;
 								map.id[x + i][y + j] = id;
 							}
-					judgeFinish();
+					while (clearRow());
 					break;
 				}
 				else{ // keeps falling
@@ -274,14 +343,31 @@ void startGame(){
 				}
 			}
 			else if (ch == 'p'){ // pause
+                hasPaused = 1;
 			}
-            else if (ch == 'c'){ // continue
+            else if (ch == 'c'){ // fall down to the bottom
+                drawSpace(shape, form, id, x, y);
+                while (isLegal(shape, form, id, x + 1, y)) x++;
+                for (int i = 0; i < 4; i++)
+                    for (int j = 0; j < 4; j++)
+                        if (block[shape][form].space[i][j]){
+                            map.data[x + i][y + j] = 1;
+                            map.color[x + i][y + j] = shape;
+                            map.id[x + i][y + j] = id;
+                        }
+                while (clearRow());
+                break;
             }
-			else if (ch == 'R'){ // restart
-				tetris();
+			else if (ch == 'R'){ // back to menu
+				// to do
 			}
 		}
 		shape = nxtShape, form = nxtForm, id++;
+        gameScore++;
+        POINT_COLOR = BLACK;
+        char gameScoreStr[10] = {0};
+        sprintf(gameScoreStr, "%d", gameScore);
+        LCD_ShowString(190, 40, 40, 30, 24, (uint8_t*) gameScoreStr);
 	}
 }
 
@@ -316,7 +402,6 @@ void draw_tetris_screen() {
 	LCD_DrawRectangle(10, 10, 80, 40);
 	LCD_DrawRectangle(90, 10, 130, 40);
 	LCD_DrawRectangle(140, 10, 180, 40);
-	LCD_DrawRectangle(190, 80, 230, 100);
 	LCD_DrawRectangle(190, 160, 230, 190);
 	LCD_DrawRectangle(190, 200, 230, 230);
 	LCD_DrawRectangle(190, 240, 230, 270);
